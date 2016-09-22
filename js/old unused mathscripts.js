44b66190923f205466728a889ec13dd4b566b519
Josh Dean <josdea@gmail.com>
2016-09-22 13:36:19 -0400

    desmos and delayed math loading
    Setting up that math loads after the page is loaded so it doesn't lock
    down the page.

--- js/old unused mathscripts.js
+++ js/old unused mathscripts.js
@@ -5,17 +5,17 @@ var solutionsVisible = true;
 var answersVisible = true;
 //toggle button for solutions
 
-function w3_open()
-{
-    document.getElementById("mySidenav").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
+// function w3_open()
+// {
+//     document.getElementById("mySidenav").style.display = "block";
+//     document.getElementById("myOverlay").style.display = "block";
+// }
 
-function w3_close()
-{
-    document.getElementById("mySidenav").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
+// function w3_close()
+// {
+//     document.getElementById("mySidenav").style.display = "none";
+//     document.getElementById("myOverlay").style.display = "none";
+// }
 
 function togglesolutions()
 {
